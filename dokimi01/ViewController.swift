f1252b89e673f4587ae953ab5f854017aa6bc447
leonidas sabanis <leonidasdev@leonidass-mac.local>
2018-04-15 00:23:39 +0300

    Initial Commit

---
+++ dokimi01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dokimi01
+//
+//  Created by leonidas sabanis on 15/04/2018.
+//  Copyright © 2018 leonidas sabanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
